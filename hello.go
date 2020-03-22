170c7b470b8f326dc16622d896f5edd04a0bd235
Philip Loeffler <philiploeffler@Philips-MacBook-Pro.local>
2020-03-22 17:51:14 -0400

    more practice

--- hello.go
+++ hello.go
@@ -55,6 +55,50 @@ if yourAge  >= 38 {
 	fmt.Println('hooray just hooray')
 }
 
+//switch case, depending on the case it will print out the necessary one
+switch yourAge {
+case 16 : fmt.println('go outside')
+case 18 : fmt.println('go inside')
+default fmt.println('go have fun')
+}
+
+// array, holds a fixed number of values of the same data type
+// defining favnumbers as an array with a length of five, yet those have not been defined
+var favNumbers[5] float64
+// here they are defined
+// the zero is saying in the 0 index the first fave number is defined at 162
+favNumbers[0] = 162
+favNumbers[1] = 11
+favNumbers[2] = 13
+favNumbers[3] = 1989
+favNumbers[4] = 52
+
+// another way to define an array
+// more fave is an array with a length of five numbers and the numbers are defined in the brackets
+moreFaveNumbers := [5]int {11, 13, 1989, 4, 52}
+
+//then to iterate over this we can do
+//the i will tell you the index and value will tell you the value of that index.
+// when using range, two value are returned for each iteration the first is the index and the second
+// is a copy of the element of that index. 
+for i, value := range moreFaveNumbers {
+	fmt.println(value, i)
+}
+
+// and if you didnt want one of the things to be returned all you would need to do is this synta
+for _, value := range moreFaveNumbers {
+	fmt.println(value)
+}
+
+//slices are like arrays but whenevr you define them you leave out the size. 
+numSlice := []int {5,4,3,2,1}
+
+// here we are creating a new slice and adding values to our new slice variable
+// the five will go to the fourth spot because as we know arrays start at 0
+// and the new slice's zero index will be the first number from the old slice, ie: 2;
+numSlice2 :=  numSlice[3:5]
+fmt.println("numSlice2[0] =", numSlice2[0]);
+
 func main() {
 	fmt.Printf("hello, world\n")
 
