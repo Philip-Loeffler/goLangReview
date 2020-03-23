6fb29f4b9604232409ff01adf08b246c768c1292
Philip Loeffler <philiploeffler@Philips-MacBook-Pro.local>
2020-03-23 11:14:04 -0400

    more practice

--- hello.go
+++ hello.go
@@ -77,8 +77,7 @@ favNumbers[4] = 52
 // more fave is an array with a length of five numbers and the numbers are defined in the brackets
 moreFaveNumbers := [5]int {11, 13, 1989, 4, 52}
 
-//then to iterate over this we can do
-//the i will tell you the index and value will tell you the value of that index.
+//then to iterate index and value will tell you the value of that index.
 // when using range, two value are returned for each iteration the first is the index and the second
 // is a copy of the element of that index. 
 for i, value := range moreFaveNumbers {
@@ -98,9 +97,53 @@ numSlice := []int {5,4,3,2,1}
 // and the new slice's zero index will be the first number from the old slice, ie: 2;
 numSlice2 :=  numSlice[3:5]
 fmt.println("numSlice2[0] =", numSlice2[0]);
+// defining a slcie without a set of values 
+// make is defining the slice of type you want, so its an array of ints
+// the five is saying that the defualt value of the first five ints will be 0 
+// and the ten is saying that the max length of the array is ten
+numSlice3 := make([]int, 5, 10)
+//if we want to add new values to this slice, we will use append
+//the variable that you put right after the append is the variable that you want to 
+//append new values to 
+numSlice3 = append(numSlice3, 0, 1)
+fmt.println(numSlice3[6]); // will print out the one
+//so here we a creating an array of strings, and since the default value is being set to 0
+// we are creating an empty array of strings
+stringSlice := make([]string, 0)
+//maps a collection of key value pairs. you're going to have a key associated with a value
+// inside the [] will be the data type for the key and the int will be the value for the values
+// we are going to store
+presAge := make(map[string] int)
+
+presAge["barack obama"] = 42
+fmt.println(presAge["barack obama"]) // this will spit out 42
+// so you put in the variable you want to use, then the key in which you want information from
+// which is barack in this case, and equals 42
+// we can then do something like this to delete a key
+presAge['kennedy'] = 43
+delete(presAge, 'kennedy')
+
+//functions
+listOfNums :=[]int{1,2,3,4,5}
+//param of ints and the int outside of the bracket is int saying, we are expecting
+// and int to be returned aka the return type
+//range again is going to cycle through whatever the paramter is and get the values
+func addThemUp(numbers []int) int {
+	sum := 0;
+	for _, val := range numbers {
+		sum += val
+	}
+	return sum
+}
+//the for loop is providing you the value of the numbers. 
+fmt.println(addThemUp(listOfNums));
+
+//here we are creating a function that is going to return 2 values
+
+
 
 func main() {
-	fmt.Printf("hello, world\n")
+	fmt.Printf("henlo, world\n")
 
 
 
